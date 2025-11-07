8ddc29d83528967f6fbf599bfcf2f134be3e4742
mads-88 <madisonhickey@u.boisestate.edu>
2025-11-07 01:19:37 -0700

    Update top.v

--- top.v
+++ top.v
@@ -6,11 +6,11 @@ module top(
     wire q1, nq1;
     d_latch u1(.D(sw[0]), .E(btnC), .Q(q1), .NotQ(nq1));
 
-    wire [7:0] memq;
-    memory_system u2(.data(sw[15:8]), .store(btnC), .addr(sw[7:6]), .q(memq));
+    wire [7:0] mem;
+    memory_system u2(.data(sw[15:8]), .store(btnC), .addr(sw[7:6]), .memory(mem));
 
     assign led[0] = q1;
     assign led[1] = nq1;
     assign led[7:2] = 6'b0;
-    assign led[15:8] = memq;
+    assign led[15:8] = mem;
 endmodule
