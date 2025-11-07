8c5680c5ab8bbf37d451b51e81899fe3f5cd1a62
mads-88 <madisonhickey@u.boisestate.edu>
2025-11-07 01:19:15 -0700

    Update memory_system.v

--- memory_system.v
+++ memory_system.v
@@ -2,7 +2,7 @@ module memory_system(
     input [7:0] data,
     input store,
     input [1:0] addr,
-    output [7:0] q
+    output [7:0] memory
 );
     wire [7:0] q0, q1, q2, q3;
     wire s0 = store & (addr==2'b00);
@@ -10,10 +10,10 @@ module memory_system(
     wire s2 = store & (addr==2'b10);
     wire s3 = store & (addr==2'b11);
 
-    byte_memory b0(.data(data), .store(s0), .q(q0));
-    byte_memory b1(.data(data), .store(s1), .q(q1));
-    byte_memory b2(.data(data), .store(s2), .q(q2));
-    byte_memory b3(.data(data), .store(s3), .q(q3));
+    byte_memory b0(.data(data), .store(s0), .memory(q0));
+    byte_memory b1(.data(data), .store(s1), .memory(q1));
+    byte_memory b2(.data(data), .store(s2), .memory(q2));
+    byte_memory b3(.data(data), .store(s3), .memory(q3));
 
     reg [7:0] y;
     always @(*) begin
@@ -24,5 +24,5 @@ module memory_system(
             2'b11: y = q3;
         endcase
     end
-    assign q = y;
+    assign memory = y;
 endmodule
